5190020694a4c2e769a4f901ddf56a0013672fb6
Saurabh Kumar <1355saurabh@gmail.com>
2024-03-10 15:08:45 +0530

    first commit

---
+++ load-more-data/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoadMoreData from "./components/load-more-data";
+
+function App() {
+	return (
+		<div className="App">
+			<LoadMoreData />
+		</div>
+	);
+}
+
+export default App;
